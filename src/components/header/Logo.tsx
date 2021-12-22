3d4b8ef6bc7b2fa9cf7368dd5456bc7384b76143
Dániel Miklós <daniel.miklos0@gmail.com>
2021-12-22 13:44:45 +0100

    header and layout

---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+/*
+		<Image
+			src="/logo.png"
+			alt="BME Management Szakkollégium logo"
+			layout="fill"
+		/>
+		*/
+
+// TODO change to svg
+export function Logo() {
+	return <p>img</p>;
+}

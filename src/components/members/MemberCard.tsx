7fcb9de555e93413dbdee3277ad7ff6ebea5b4e9
Dániel Miklós <daniel.miklos0@gmail.com>
2021-12-22 23:52:41 +0100

    members card with example

--- src/components/members/MemberCard.tsx
+++ src/components/members/MemberCard.tsx
@@ -9,7 +9,7 @@ type MemberCardType = {
 export function MemberCard({ name, image, position }: MemberCardType) {
 	return (
 		<div className="flex flex-col items-center">
-			<div className="relative flex-shrink-0 w-80 h-80">
+			<div className="relative flex-shrink-0 w-40 md:w-60 lg:w-80 h-40 md:h-60 lg:h-80">
 				<Image
 					src={image}
 					layout="fill"
@@ -17,8 +17,10 @@ export function MemberCard({ name, image, position }: MemberCardType) {
 					className="rounded-full"
 				/>
 			</div>
-			<p>{name}</p>
-			<p>{position}</p>
+			<div className="flex flex-col items-center my-4 text-2xl">
+				<p>{name}</p>
+				<p>{position}</p>
+			</div>
 		</div>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,23 @@ export default function Page() {
 	return (
 		<div>
 			<p>asd</p>
-			<MemberCard image="http://placekitten.com/200/300" name="Kiss József" />
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+				<MemberCard
+					image="http://placekitten.com/200/300"
+					name="Kiss József"
+					position="elnök"
+				/>
+				<MemberCard
+					image="http://placekitten.com/200/300"
+					name="Kiss József"
+					position="elnök"
+				/>
+				<MemberCard
+					image="http://placekitten.com/200/300"
+					name="Kiss József"
+					position="elnök"
+				/>
+			</div>
 		</div>
 	);
 }

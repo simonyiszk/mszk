0fde01f70e651989a24f5c06c92aa5ca7bfcb155
Miklos Daniel <46491285+perryd01@users.noreply.github.com>
2021-12-23 13:48:28 +0100

    Member component (#16)
    * member card
    
    * members card with example

---
+++ src/components/members/MemberCard.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+type MemberCardType = {
+	name: string;
+	image: string;
+	position?: string;
+};
+// TODO replace to CDN interface
+export function MemberCard({ name, image, position }: MemberCardType) {
+	return (
+		<div className="flex flex-col items-center">
+			<div className="relative flex-shrink-0 w-40 md:w-60 lg:w-80 h-40 md:h-60 lg:h-80">
+				<Image
+					src={image}
+					layout="fill"
+					objectFit="cover"
+					className="rounded-full"
+				/>
+			</div>
+			<div className="flex flex-col items-center my-4 text-2xl">
+				<p>{name}</p>
+				<p>{position}</p>
+			</div>
+		</div>
+	);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import { MemberCard } from "@/components/members/MemberCard";
+
 export default function Page() {
 	return (
 		<div>
 			<p>asd</p>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+				<MemberCard
+					image="http://placekitten.com/200/300"
+					name="Kiss József"
+					position="elnök"
+				/>
+				<MemberCard
+					image="http://placekitten.com/200/300"
+					name="Kiss József"
+					position="elnök"
+				/>
+				<MemberCard
+					image="http://placekitten.com/200/300"
+					name="Kiss József"
+					position="elnök"
+				/>
+			</div>
 		</div>
 	);
 }

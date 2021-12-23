33ac5ecc61433dc5805758504dcbe9084eda4f44
Dániel Miklós <daniel.miklos0@gmail.com>
2021-12-23 13:52:24 +0100

    card to index

---
+++ src/components/sponsors/SponsorCard.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+type SponsorCardProps = {
+	name: string;
+	image: string;
+	description?: string;
+};
+
+export function SponsorCard({ name, image, description }: SponsorCardProps) {
+	return (
+		<div className="flex sm:flex-row gap-6 items-center p-4 w-full bg-gray-100 rounded-lg shadow-mszk-sponsor">
+			<div className="relative w-32 h-32">
+				<Image
+					src={image}
+					layout="fill"
+					objectFit="cover"
+					className="flex-shrink-0"
+				/>
+			</div>
+			<div>
+				<h1 className="text-2xl font-medium">{name}</h1>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
+}

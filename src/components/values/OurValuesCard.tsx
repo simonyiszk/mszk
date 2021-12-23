63896082506bcfef294bd2f0ef8af3222d1b9c8d
Miklos Daniel <46491285+perryd01@users.noreply.github.com>
2021-12-23 22:06:57 +0100

    fix width (#26)

--- src/components/values/OurValuesCard.tsx
+++ src/components/values/OurValuesCard.tsx
@@ -26,20 +26,19 @@ export function OurValuesCard({
 	number,
 }: OurValuesCardProps) {
 	return (
-		<div className="relative p-8 w-96 h-80 rounded-md shadow-lg">
+		<div className="relative p-8 w-full max-w-sm h-96 rounded-md shadow-lg">
 			<div className="relative w-24 h-28">
 				<Image src={iconSrc[icon]} layout="fill" priority />
 			</div>
 			<div className="my-4">
 				<h1 className="text-3xl font-medium">{title}</h1>
-				<p className="mt-6">{description}</p>
+				<p className="mt-6 text-xl">{`${description.slice(0, 82)}...`}</p>
 			</div>
 			{/* https://daily-dev-tips.com/posts/making-gradient-text-with-tailwind-css/ */}
 			<span className="absolute top-0 right-0 -mt-12 text-11xl font-black text-transparent bg-clip-text bg-gradient-to-b from-gray-200 to-transparent select-none">
 				{number}
 			</span>
-			{/* TODO REVIEW ME */}
-			<div className="flex justify-center items-center mx-auto -mt-5 w-16 h-16 bg-mszk-blue rounded-full">
+			<div className="flex justify-center items-center mx-auto mt-12 w-16 h-16 bg-mszk-blue rounded-full">
 				<span className="text-6xl text-white">+</span>
 			</div>
 		</div>

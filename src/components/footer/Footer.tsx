737d7171afaf3d5bda34c82c34898a4362eef45b
Dániel Miklós <daniel.miklos0@gmail.com>
2022-02-13 13:27:59 +0100

    fix line height and img

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export function Footer() {
 	return (
@@ -7,19 +8,27 @@ export function Footer() {
 				<div className="flex flex-col xl:flex-row gap-2 xl:gap-16 justify-center w-full">
 					<div className="text-sm xl:text-base font-light text-center">
 						<div className="flex flex-row justify-center my-4">
-							<div className="relative w-64 h-16">
-								<Image src="/transparent_logo.png" layout="fill" />
-							</div>
+							<Image src="/transparent_logo.png" width={336} height={78} />
 						</div>
-						<h1 className="text-xl font-normal">BME Managment Szakkollégium</h1>
-						<p>1126 Budapest, Tartsay Vilmos utca 20. II. em. 1.a.</p>
-						<p>Bankszámlaszám:</p>
-						<p>K&H 10400164-01600249-00000000</p>
-						<p>Adószám:</p>
-						<p>19020824-1-43</p>
-						<p className="text-xs lg:text-base">
-							főoldal | projektjeink | jelentkezz | mszk35 | kapcsolat |
-							támogatás{" "}
+						<p className="text-xl font-normal">BME Managment Szakkollégium</p>
+						<p className="mt-1">
+							1126 Budapest, Tartsay Vilmos utca 20. II. em. 1.a.
+						</p>
+						<div className="my-1 leading-4">
+							<p>Bankszámlaszám:</p>
+							<p>K&H 10400164-01600249-00000000</p>
+						</div>
+						<div className="my-1 leading-4">
+							<p>Adószám:</p>
+							<p>19020824-1-43</p>
+						</div>
+						<p className="my-4 text-xs lg:text-base">
+							<Link href="/">főoldal</Link> |{" "}
+							<Link href="/projekjeink">projektjeink</Link> |{" "}
+							<Link href="/jelentkezz">jelentkezz</Link> |{" "}
+							<Link href="/mszk35">mszk35</Link> |{" "}
+							<Link href="/kapcsolat">kapcsolat</Link> |{" "}
+							<Link href="/tamogatas">támogatás </Link>
 						</p>
 					</div>
 				</div>

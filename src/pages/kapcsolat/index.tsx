b7a84a04100c8fa7ac1f0b18ff741060669c09cb
Dániel Miklós <daniel.miklos0@gmail.com>
2022-03-06 20:32:13 +0100

    fb page link

--- src/pages/kapcsolat/index.tsx
+++ src/pages/kapcsolat/index.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import Image from "next/image";
 
 import { InnerLayout } from "@/components/layouts/InnerLayout";
@@ -6,11 +7,13 @@ export default function ContactPage() {
 	const phone = "36-1-463-1095";
 	const address =
 		"1111 Budapest, Műegyetem rakpart 3. (K épület) 3. emelet 84.";
+	const fb = "https://www.facebook.com/MSZKofficial";
 	return (
 		<div className="mb-16">
 			<div className="relative w-full">
 				<div className="relative z-0 w-full h-72 bg-cover bg-contact-img" />
-				<div className="flex absolute z-10 flex-col gap-4 p-8 mx-6 -mt-16 bg-white rounded-lg shadow-lg">
+				{/* card */}
+				<div className="flex absolute z-10 flex-col gap-4 p-8 -mt-16 bg-white rounded-lg shadow-lg">
 					<div className="flex flex-col">
 						<span className="text-sm text-mszk-blue uppercase">Címünk</span>
 						<a
@@ -33,16 +36,29 @@ export default function ContactPage() {
 							{phone}
 						</a>
 					</div>
+					<div className="flex flex-col">
+						<span className="text-sm text-mszk-blue uppercase">
+							Facebook oldalunk
+						</span>
+						<a
+							href={fb}
+							target="_blank"
+							className="text-lg font-bold cursor-pointer"
+							rel="noreferrer"
+						>
+							{fb.replace("https://www.facebook.com/", "@")}
+						</a>
+					</div>
 				</div>
 			</div>
 
 			<InnerLayout>
-				<div className="mt-48">
+				<div className="mt-64">
 					<div className="my-8">
 						<h1 className="text-2xl font-light text-center uppercase">
 							Miért érdemes velünk együttműködni?
 						</h1>
-						<p className="my-8">
+						<p className={clsx(true && "my-8")}>
 							Az EVK Szakkollégium a legjobb magyar business szakkollégiumként
 							kiemelt kapcsolatot ápol a hazai üzleti szféra jeles
 							képviselőivel. Fontos célunk, hogy vállalati együttműködéseink

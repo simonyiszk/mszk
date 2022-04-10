0c62f9a831c0faf31efb0bb723a8d2920f2a78d8
Miklós Dániel <daniel.miklos0@gmail.com>
2022-04-10 21:51:58 +0200

    kapcsolat oldal

--- src/pages/kapcsolat/index.tsx
+++ src/pages/kapcsolat/index.tsx
@@ -13,51 +13,58 @@ export default function ContactPage() {
 			<div className="relative w-full">
 				<div className="relative z-0 w-full h-72 bg-cover bg-contact-img" />
 				<div className="flex absolute right-0 left-0 z-10 flex-col gap-4 p-8 mx-6 lg:mx-32 -mt-16 bg-white rounded-lg shadow-lg">
-					<div className="flex flex-col">
-						<span className="text-sm text-mszk-blue uppercase">Címünk</span>
-						<a
-							target="_blank"
-							rel="noopener noreferrer"
-							className="text-lg font-bold cursor-pointer"
-							href={`https://maps.google.com/?=${address}`}
-						>
-							{address}
-						</a>
-					</div>
-					<div className="flex flex-col">
-						<span className="text-sm text-mszk-blue uppercase">
-							Telefonszámunk
-						</span>
-						<a
-							href={`tel:${phone}`}
-							className="text-lg font-bold cursor-pointer"
-						>
-							{phone}
-						</a>
-					</div>
-					<div className="flex flex-col">
-						<span className="text-sm text-mszk-blue uppercase">
-							Facebok oldalunk
-						</span>
-						<a
-							target="_blank"
-							rel="noopener noreferrer"
-							className="text-lg font-bold cursor-pointer"
-							href={fb}
-						>
-							{fb.replace("https://www.", "")}
-						</a>
+					<div className="flex flex-col md:flex-row gap-16 justify-center">
+						<div className="hidden md:block relative w-32 h-32">
+							<Image src="/icons/anchor.svg" layout="fill" />
+						</div>
+						<div>
+							<div className="flex flex-col">
+								<span className="text-sm text-mszk-blue uppercase">Címünk</span>
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-lg font-bold cursor-pointer"
+									href={`https://maps.google.com/?=${address}`}
+								>
+									{address}
+								</a>
+							</div>
+							<div className="flex flex-col">
+								<span className="text-sm text-mszk-blue uppercase">
+									Telefonszámunk
+								</span>
+								<a
+									href={`tel:${phone}`}
+									className="text-lg font-bold cursor-pointer"
+								>
+									{phone}
+								</a>
+							</div>
+							<div className="flex flex-col">
+								<span className="text-sm text-mszk-blue uppercase">
+									Facebok oldalunk
+								</span>
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-lg font-bold cursor-pointer"
+									href={fb}
+								>
+									{fb.replace("https://www.", "")}
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 
 			<InnerLayout>
-				<div className="mt-48">
+				<div className="mt-64">
 					<div className="my-8">
 						<h1 className="text-2xl font-light text-center uppercase">
 							Miért érdemes velünk együttműködni?
 						</h1>
-						<p className="my-8">
+						<p className="my-16">
 							Az EVK Szakkollégium a legjobb magyar business szakkollégiumként
 							kiemelt kapcsolatot ápol a hazai üzleti szféra jeles
 							képviselőivel. Fontos célunk, hogy vállalati együttműködéseink
@@ -76,7 +83,7 @@ export default function ContactPage() {
 								objectFit="cover"
 							/>
 						</div>
-						<p className="my-4">
+						<p className="my-8">
 							Ha személyesen beszélgetnél velünk, akkor a BME K épületében járva
 							nyugodtan gyere be K384-es számú irodánkba! Viszont ha gyorsan
 							üzennél nekünk, akkor ezt is megteheted akár Facebookon, akár
